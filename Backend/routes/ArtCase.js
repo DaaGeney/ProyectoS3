85acd41f8781d616fd1e27c206c5f5ba584ddfcc
Diego Assia <diegoassia@gmail.com>
2019-08-25 13:33:04 -0500

    Http methods request

--- Backend/routes/ArtCase.js
+++ Backend/routes/ArtCase.js
@@ -6,8 +6,48 @@ const db = mongojs('DatabaseArtcase',[
 
 module.exports = app => {
     app.get('/ArtCase',(req,res)=>{
+       db.artCase.find((err,artCase)=>{
         res.json({
-            response:'Funciona artCase'
+            response: artCase
+        })
+       })
+    })
+
+    app.post('/ArtCase', (req,res)=>{
+        let newArtcase =req.body;
+        console.log(req.body);
+        db.artCase.insert(newArtcase, (err, response)=>{
+            res.json({
+                //artCase
+                response
+            })
+        })
+        
+    })
+
+    app.put('/artcase/:id',(req,res) =>{
+        let updateArtcase = req.body
+        db.artCase.update(
+            {_id: mongojs.ObjectId(req.params.id)},
+            updateArtcase,
+            {},
+            (err, response)=>{
+                res.json({
+                    response
+                })
+            }
+        )
+    })
+
+    app.delete('/artcase/:id', (req,res)=>{
+        db.artCase.remove({
+            _id: mongojs.ObjectId(req.params.id)
+        }, (err,response)=>{
+            res.json({
+                response: 'Eliminado de nuevo'
+            })
         })
     })
+
+
 }
