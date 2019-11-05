f23b5a2bdad33e24f6df59d7cf280b5c7b760895
Diego Assia <diegoassia@gmail.com>
2019-11-05 13:18:32 -0500

    Test

---
+++ ArtCase/Project/src/test/hello-name.spec.js
@@ -0,0 +1,10 @@
+import helloName from '@/hello-name'
+
+
+
+describe('Tests File hello-name.js', () => {
+    it('Should get Hello Jose', () => {
+         const name = 'Jose'
+         const message = helloName(name)
+         expect(message).toEqual('Hello ' + name)})
+})
